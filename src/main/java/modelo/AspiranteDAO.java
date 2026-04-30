aec517e4980fc51ea512211172c9260562f215bc
cam <jucvargasc@udistrital.edu.co>
2026-04-30 16:12:39 -0500

    vc_origen

---
+++ src/main/java/modelo/AspiranteDAO.java
@@ -0,0 +1,11 @@
+package modelo;
+
+// Julian Camilo Vargas Carvajal
+// DAO en memoria para almacenar los aspirantes registrados.
+
+import java.util.ArrayList;
+
+public class AspiranteDAO {
+
+	public static ArrayList<Aspirante> lista_A = new ArrayList<Aspirante>();
+}
